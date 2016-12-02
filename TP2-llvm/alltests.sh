5e5c2607aa599a112ea0e29b99cc80e268a26b24
Aurele-Barriere <aurele.barriere@ens-rennes.fr>
2016-12-02 09:44:15 +0100

    testing script

---
+++ TP2-llvm/alltests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for f in tests/testlevel4/*.vsl
+do
+    printf "\n\n------------------------------------------------\n\n"
+    echo $f
+    cat $f
+    echo
+    nf=${f%.vsl}
+    ./compile.sh $nf
+    ./$nf
+done
+
+# calling void function (see testlevel2/level2expr.vsl)
+# tableaux
+# probleme de scope : testleve4t1.vsl
+# boucle infinie : testlevel4testcarre.vsl
