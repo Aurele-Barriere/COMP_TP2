5533f234adbd74013fa397e2402f61e1456afe4f
Aurele-Barriere <aurele.barriere@gmail.com>
2016-12-09 15:55:47 +0100

    it's better to draw rectangles

--- TP2-llvm/plot.py
+++ TP2-llvm/plot.py
@@ -2,20 +2,25 @@ from PIL import Image
 from PIL import ImageDraw
 import numpy as np              
 import matplotlib.pyplot as plt
-
-def drawline(x1,y1,x2,y2):
-    print ("1")
-
+import matplotlib.patches as patches
+from matplotlib.patches import Rectangle
 
 with open('data') as f:
     content = f.readlines()
 
-    im = Image.new('RGBA',(1000,1000),(255,255,255,255))
-    draw = ImageDraw.Draw(im)
+    #im = Image.new('RGBA',(1000,1000),(255,255,255,255))
+    #draw = ImageDraw.Draw(im)
+    fig = plt.figure()
+    axis = plt.gca()
+    axis.set_xlim([0,1000])
+    axis.set_ylim([0,1000])
     for line in content:
         points = line.split()
-        if (len(points) == 4):
-            draw.line((float(points[0]),float(points[1]),float(points[2]),float(points[3])), fill=(0,0,0,0),width=3)
+        if (len(points) == 3):
+            x = int(float(points[0]))
+            y = int(float(points[1]))
+            l = int(float(points[2]))
+            axis.add_patch(Rectangle((x,y),l,l,alpha=1))
 
-    plt.imshow(im)
+    #plt.imshow(im)
     plt.show()
