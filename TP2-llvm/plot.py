808a1e8ca53b2b82678cd1e8449755398b2c3feb
Aurele-Barriere <aurele.barriere@gmail.com>
2016-12-09 13:03:27 +0100

    added plot in python

---
+++ TP2-llvm/plot.py
@@ -0,0 +1,21 @@
+from PIL import Image
+from PIL import ImageDraw
+import numpy as np              
+import matplotlib.pyplot as plt
+
+def drawline(x1,y1,x2,y2):
+    print ("1")
+
+
+with open('data') as f:
+    content = f.readlines()
+
+    im = Image.new('RGBA',(1000,1000),(255,255,255,255))
+    draw = ImageDraw.Draw(im)
+    for line in content:
+        points = line.split()
+        if (len(points) == 4):
+            draw.line((float(points[0]),float(points[1]),float(points[2]),float(points[3])), fill=(0,0,0,0),width=3)
+
+    plt.imshow(im)
+    plt.show()
